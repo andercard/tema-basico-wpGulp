1e9356d5ec325341708c5ca93fcb49a1a63d4152
Andersson Mesa <andercard66@gmail.com>
2017-07-19 21:39:21 -0500

    tema basico

--- footer.php
+++ footer.php
@@ -1,4 +1,11 @@
-    </main>
-    <footer>Desarrolado por: Andersson Mesa</footer>
-  </body>
+    <!-- Footer -->
+    <footer id="footer">
+      <ul class="copyright">
+        <li>&copy; Andersson Mesa</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+      </ul>
+    </footer>
+
+   </div>
+   <?php wp_footer(); ?>
+	</body>
 </html>
